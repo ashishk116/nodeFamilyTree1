6f56e7dd645f17034fdb05629bf0f3de746d68af
ashishk116 <ashishkumbhare116@gmail.com>
2020-11-09 15:03:09 +0530

    signin/out

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const User =require('../models/user')
 const { check,validationResult } = require('express-validator');
-
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
 exports.signup=(req,res)=>{
     const user=new User(req.body);
@@ -10,15 +11,12 @@ exports.signup=(req,res)=>{
             error:errors.array()[0].msg
         })
     }
-
-
     user.save((err,user)=>{
         if(err){
             return res.status(400).json({
                 err:"not able to save user in DB"
             })
         }
-
     res.json({
             name:user.name,
             email:user.email,
@@ -27,7 +25,64 @@ exports.signup=(req,res)=>{
     })
 }
 
+
+exports.signin=(req,res)=>{
+    const{email,password} =req.body
+    const errors=validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error:errors.array()[0].msg
+        });
+    }
+
+
+    User.findOne({email},(err,user)=>{
+        if(err || !user){
+           return res.status(400).json({
+                error:"User email doesnot exist"
+            })
+        }
+
+        if(!user.authenticate(password)){
+           return res.status(401).json({
+            error:"Email and password doesnot match"
+            })
+        }
+
+//CREATE TOKEN
+        const token = jwt.sign({ _id:user._id}, process.env.SECRET);
+
+        //PUT TOKEN IN COOKIE
+
+        res.cookie("token",token,{expire :new Date() +9999})
+
+        //send response to frontend
+
+        const{_id,name,email,role}=user;
+        return res.json({token,user:{_id,name,email,role}})
+
+    })
+}
+
+exports.getAllUsers=(req,res)=>{
+    User.find().exec((err,users)=>{
+
+        if(err || !users){
+            return res.status(400).json({
+                error:"No users found"
+            })
+        }
+
+        res.json(users);
+    })
+}
+
+
+
 exports.signout =(req,res)=>{
+
+    res.clearCookie("token")
     res.json({
         message:"Signout works"
     })
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 var express = require('express')
 const { check } = require('express-validator');
 var router = express.Router()
-const {signout,signup} =require('../controllers/auth')
+const {signout,signup,signin,getAllUsers} =require('../controllers/auth')
 
 
 
@@ -13,6 +13,20 @@ check("password","password should be 3char").isLength({min:3}),
 ]
 
 ,signup),
+
+
+
+router.post("/signin",
+[
+check("email","email is required").isEmail(),
+check("password","password field is required").isLength({min:3}),
+]
+
+,signin),
+
+
+
+router.get("/users",getAllUsers)
 
 router.get("/signout",(req, res) => {
     res.send('Signout !')
