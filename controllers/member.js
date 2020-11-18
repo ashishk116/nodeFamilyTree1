3f281c9b6e10b47d10de4796d0767183fdfa3b46
ashishk116 <ashishkumbhare116@gmail.com>
2020-11-18 12:25:14 +0530

    members added

--- controllers/member.js
+++ controllers/member.js
@@ -1,4 +1,9 @@
 const Member = require('../models/member');
+const formidable = require("formidable");
+const _ =require("lodash");
+const fs=require("fs")
+
+
 
 exports.getMemberById=(req,res,next,id)=>{
     Member.findById(id)
@@ -12,4 +17,53 @@ exports.getMemberById=(req,res,next,id)=>{
         req.member=member;
         next();
     })
+}
+
+exports.createMember=(req,res)=>{
+let form= new formidable.IncomingForm()
+form.keepExtensionse=true;
+
+form.parse(req,(err,fields,file)=>{
+
+
+    if(err){
+        return res.status(400).json({
+            error:"problem with image"
+        })
+    }
+    //destructuring the fields
+    const{name,familytree,relation}=fields;
+
+    if(!name||!familytree||!relation){
+        res.status(400).json({
+            error:"Please include all fields"
+        })
+    }
+
+    let member= new Member(fields)
+if(file.photo){
+    if(file.photo.size>3000000){
+        return res.status(400).json({
+            error:"photo file is too big!!"
+        })
+    }
+
+    member.photo.data=fs.readFileSync(file.photo.path)
+    member.photo.contentType=file.photo.type
+}
+
+//save to DB
+
+member.save((err,member)=>{
+    if(err){
+        res.status(400).json({
+            error:"saving Db is failed"
+        })
+    }
+
+    res.json(member);
+})
+
+});
+
 }
--- routes/member.js
+++ routes/member.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const {getMemberById}= require('../controllers/member')
+const {getMemberById,createMember}= require('../controllers/member')
 const {isSignedIn,isAuthenticated,isAdmin}= require('../controllers/auth')
 const {getUserById}= require('../controllers/user')
 
-
+//all of param
 router.param("userId",getUserById);
 router.param("memberId",getMemberById);
 
+
+//actual routes
+router.post("/member/create/:userId",isSignedIn,isAuthenticated,createMember);
 
 
 module.exports= router;
